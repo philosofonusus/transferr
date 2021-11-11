cb2ca378c901dccd6c640bbcc14a3ab35513ffb1
tentacle <my.tentacles.are.in.you@gmail.com>
2021-11-11 21:13:13 +0200

    s

--- index.js
+++ index.js
@@ -55,7 +55,6 @@ const write_data = async (toCard, amount, fromCard,cvv, expireDate, email, id) =
     await page.waitForNavigation({waitUntil: 'networkidle2'});
 
     const input = await page.$$('input')[0]
-    console.log(input.innerHTML)
     page.$eval('input', (element) => {
       console.log(element.innerHTML)
     })
